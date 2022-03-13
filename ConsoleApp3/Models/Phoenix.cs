55368404858a663732b44f80d3df45a93b4bf825
AlFeer <tu6epg9t8@code.edu.az>
2022-03-13 21:48:35 +0400

    first commit

---
+++ ConsoleApp3/Models/Phoenix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixSuns
+{
+    public class Phoenix //NBA team roster
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public int ppg;// ppg - point per game
+        public int shirtNumber;
+        public Phoenix(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+        public Phoenix(string name, string surname, int age, int ppg) : this(name, surname, age)
+        {
+            this.ppg = ppg;
+        }
+        public Phoenix(string name, string surname, int age, int ppg, int shirtNumber) : this(name, surname, age, ppg)
+        {
+            this.shirtNumber = shirtNumber;
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using PhoenixSuns;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your player: ");
+            Phoenix playername = new Phoenix(Convert.ToString(Console.ReadLine()), Convert.ToString(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()), Convert.ToInt32(Console.ReadLine()));
+        } 
+    }
+}
